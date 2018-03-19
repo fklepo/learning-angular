abf3c8c3f2a7152c1b45e7a0f4f145f62a3a9434
filipklepo <filipklepo@gmail.com>
2018-03-19 16:30:47 +0100

    Add PageNotFoundComponent. Move Todo interface into separate file.

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: '<h1>404 - Page Not Found</h1>'
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
